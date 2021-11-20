b6ca63c331aa989f316769bfc03f5b5e495d5177
gungla <gglahn@gmail.com>
2021-11-20 18:04:32 -0300

    Actualizo deply

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
